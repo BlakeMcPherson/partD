a57cb5ffe4b985b7be1ec3ac61b0b1c474989b3b
BlakeMcPherson <70533767+BlakeMcPherson@users.noreply.github.com>
2020-09-30 03:14:03 +0200

    commit transportation

--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ public class Main
 	public static void main(String[] args) {
 		System.out.println("Hi, my name is Blake");
 		System.out.println("My favourite colour is Blue");
+		System.out.println("My favourite mode of transportation is Car");
 	}
 }
